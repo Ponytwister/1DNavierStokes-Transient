79ccb7bf3c8ddb69b3343a87229f3fe73b76b2d6
Ponytwister <The.eric.johnston@gmail.com>
2024-08-08 17:25:04 -0500

    Initial Modifications

--- NavierSolver1Dtransient One Stage Reaction.cpp
+++ NavierSolver1Dtransient One Stage Reaction.cpp
@@ -8,12 +8,12 @@
 // ND: Nanoparticle Bound Dye
 
 using namespace std;
-void SetInletCondit(float* D, float* DD, float* ND, float* NS, float* NDD, int z = 0);
-void SaveViewable(float* V, int X, int Y, int Z, string Filename);
-void SaveExcelOutput(int z, float* D, float* DD, float* ND, float* NDD, float* NS);
+void SetInletCondit(float* D, float* ND, float* NS, int z = 0);
 
-const int Z                 = 100;
-const int X                 = 400;
+void SaveExcelOutput(int z, float* D, float* ND, float* NS, float p, float kon);
+
+const int Z                 = 100; // divisions along the z(time) axis
+const int X                 = 400; // divisions along the x axis
 const int M                 = X * Z;
 
 // Device Dimensions
@@ -22,28 +22,25 @@ const float H = 4e-5;  //meters: 40 um
 const float L = 0.025; //meters: 2.5 cm
 
 // Operating conditions/settings
-const float Flow            = 2.0f * 5e-9f / 60;                        //m3/s: 2*5 ulmin
-const float diameter        = 23e-9f;
-float BeadConc              = 0.001f * 6.0f / (1.05f * diameter); // m2/m3
-float DyeConc               = 0.0033f * 1000.0f / 332.31f;     // mol/m3 
+const float flow            = 2.0f * 5e-9f / 60;                        //m3/s: 2*5 ulmin
+const float diameter        = 41e-9f; // meters
+const float wt_percent      = 0.1f; // wt%
+const float dye_conc_mgml   = 0.00336f; // mg/ml FITC
+const float bead_conc             = wt_percent / 100.0f / 1.05f / ( 4.0f / 3.0f * M_PI * powf(diameter / 2.0f, 3.0f)); // beads/m3 
+const float dye_conc              = dye_conc_mgml * 1000.0f / 332.326f * 6.022e+23;     // molecules FITC / m3 
 
 // Physical Constants
-const float P1              = 4.59e-10f / DyeConc * BeadConc; //FITC mol / PS area m2
-const float P2              = 4.59e-10f / DyeConc * BeadConc; //FITC mol / PS area m2
-const float Keq             = 1422.0f;
-float Kon1                  = 0 * 100.8f * (W / X) * DyeConc * BeadConc; // 0.10f;
-float Kon2                  = 0.0f; //324.8f * (W / X) * DyeConc * BeadConc; // 0.10f;
-const float Visc            = 0.0010016f;              //Dynamic viscosity of water at 20C in Pa.s
-const float Difusion_dye    = 4.9e-10f; // m2/s From 4.9 × 10−6 cm2 s−1 The diffusion coefficient of fluorescein in water at 21.5°C, as calculated from the Wilke-Chang correlation
-const float Difusion_beads  = 1.380649e-23f * (273.15f + 25.0f) / (3.0f * M_PI * Visc * diameter); // m2/s From kB*T/(3*pi*visc*d) kB=1.380649×10−23 J⋅K−1
+const float visc            = 0.0010016f; // Dynamic viscosity of water at 20C in Pa.s
+const float difusion_dye    = 4.9e-10f; // m2/s From 4.9 × 10−6 cm2 s−1 The diffusion coefficient of fluorescein in water at 21.5°C, as calculated from the Wilke-Chang correlation
+const float difusion_beads  = 1.380649e-23f * (273.15f + 25.0f) / (3.0f * M_PI * visc * diameter); // m2/s From kB*T/(3*pi*visc*d) kB=1.380649×10−23 J⋅K−1
 
 // Derived values for Crank-Nicolson implicit method
-const float restime       = W * H * L / Flow; // seconds
+const float restime       = W * H * L / flow; // seconds
 const float dt            = restime / Z; // seconds
-const float T_dye         = Difusion_dye * restime / (W * W);
-const float T_beads       = Difusion_beads * restime / (W * W);
-const float dT_dye        = Difusion_dye * dt / (W * W); // T = Dt/l^2
-const float dT_beads      = Difusion_beads * dt / (W * W);
+const float T_dye         = difusion_dye * restime / (W * W);
+const float T_beads       = difusion_beads * restime / (W * W);
+const float dT_dye        = difusion_dye * dt / (W * W); // T = Dt/l^2
+const float dT_beads      = difusion_beads * dt / (W * W);
 const float dx            = W / X; // m / subdivision
 const float dX            = 1.0f / X; // X = x/l
 const float r_dye         = dT_dye / dX / dX; // r = dT/(dX)^2
@@ -65,14 +62,12 @@ main()
 
     //The 3 Concentration Arrays.
     float* D{new float[M]{}};
-    float* DD{new float[M]{}};
     float* ND{new float[M]{}};
-    float* NDD{new float[M]{}};
     float* NS{new float[M]{}};
 
     //initialize the 3 Concentration Arrays.
     for (int z = 0; z < Z; z++) {
-        SetInletCondit(D, DD, ND, NS, NDD, z);
+        SetInletCondit(D, ND, NS, z);
     }
 
     Eigen::MatrixXf m(X,X);
@@ -81,24 +76,17 @@ main()
     m_beads.setZero();
     Eigen::MatrixXf E(X,1);
     E.setZero();
-    Eigen::MatrixXf E_DD(X,1);
-    E_DD.setZero();
     Eigen::MatrixXf E_NS(X,1);
     E_NS.setZero();
     Eigen::MatrixXf E_ND(X,1);
     E_ND.setZero();
-    Eigen::MatrixXf E_NDD(X,1);
-    E_NDD.setZero();
     Eigen::MatrixXf Solution(X,1);
     Solution.setZero();
-    Eigen::MatrixXf Solution_DD(X,1);
-    Solution_DD.setZero();
     Eigen::MatrixXf Solution_NS(X,1);
     Solution_NS.setZero();
     Eigen::MatrixXf Solution_ND(X,1);
     Solution_ND.setZero();
-    Eigen::MatrixXf Solution_NDD(X,1);
-    Solution_NDD.setZero();
+
 
     for (int i = 0; i < X; i++) {
         for (int j = 0; j < X; j++) {
@@ -123,119 +111,89 @@ main()
     m = m.inverse();
     m_beads = m_beads.inverse();
 
-    float reactionrate1 = 0.0f;
-    float reactionrate2 = 0.0f;
+    float P1              = 100.0f; // FITC molecules / PS bead
+    //const float Keq       = 1422.0f;
+    float kon1            = 1.0E-10f * (W / X); // 0.10f;
+    float reaction_rate_1 = 0.0f;
 
-    for (int z = 1; z < Z; z++) {
-        for (int i = 0; i < X; i++) {
-            reactionrate1 = Kon1 * dt * (D[i + (z - 1) * X] * NS[i + (z - 1) * X] - ND[i + (z - 1) * X] / Keq);
-            reactionrate2 = 0.0f; //Kon2 * dt * (DD[i + (z - 1) * X] * NDD[i + (z - 1) * X]);
-
-            if (i == 0) {
-                E(i,0)      = (1.00f - r_dye)   * D[i + (z - 1) * X]    + r_dye   * D[i + (z - 1) * X + 1]                                                              - reactionrate1;
-                E_DD(i,0)   = (1.00f - r_beads) * DD[i + (z - 1) * X]   + r_beads * DD[i + (z - 1) * X + 1]                                                             + reactionrate1      - reactionrate2;
-                E_NS(i,0)   = (1.00f - r_beads) * NS[i + (z - 1) * X]   + r_beads * NS[i + (z - 1) * X + 1]                                                             - reactionrate1 / P1;
-                E_NDD(i,0)  = (1.00f - r_beads) * NDD[i + (z - 1) * X]  + r_beads * NDD[i + (z - 1) * X + 1]                                                            + reactionrate1 / P1 - reactionrate2 / P2;
-                E_ND(i,0)   = (1.00f - r_beads) * ND[i + (z - 1) * X]   + r_beads * ND[i + (z - 1) * X + 1]                                                             + reactionrate1 / P1;
-                
-            } else if (i == X - 1) {
-                E(i,0)      =                                           r_dye   * D[i + (z - 1) * X - 1]                    + (1.00f - r_dye)   * D[i + (z - 1) * X]    - reactionrate1;
-                E_DD(i,0)   =                                           r_beads * DD[i + (z - 1) * X - 1]                   + (1.00f - r_beads) * DD[i + (z - 1) * X]   + reactionrate1 - reactionrate2;
-                E_NS(i,0)   =                                           r_beads * NS[i + (z - 1) * X - 1]                   + (1.00f - r_beads) * NS[i + (z - 1) * X]   - reactionrate1 / P1;
-                E_NDD(i,0)  =                                           r_beads * NDD[i + (z - 1) * X - 1]                  + (1.00f - r_beads) * NDD[i + (z - 1) * X]  + reactionrate1 / P1 - reactionrate2 / P2;
-                E_ND(i,0)   =                                           r_beads * ND[i + (z - 1) * X - 1]                   + (1.00f - r_beads) * ND[i + (z - 1) * X]   + reactionrate1 / P1;
-            } else {
-                E(i,0)      = r_dye   * D[i + (z - 1) * X - 1]          + (2.00f - 2.00f * r_dye)   * D[i + (z - 1) * X]    + r_dye   * D[i + (z - 1) * X + 1]          - reactionrate1;
-                E_DD(i,0)   = r_beads * DD[i + (z - 1) * X - 1]         + (2.00f - 2.00f * r_beads) * DD[i + (z - 1) * X]   + r_beads * DD[i + (z - 1) * X + 1]         + reactionrate1 - reactionrate2;
-                E_NS(i,0)   = r_beads * NS[i + (z - 1) * X - 1]         + (2.00f - 2.00f * r_beads) * NS[i + (z - 1) * X]   + r_beads * NS[i + (z - 1) * X + 1]         - reactionrate1 / P1;
-                E_NDD(i,0)  = r_beads * NDD[i + (z - 1) * X - 1]        + (2.00f - 2.00f * r_beads) * NDD[i + (z - 1) * X]  + r_beads * NDD[i + (z - 1) * X + 1]        + reactionrate1 / P1 - reactionrate2 / P2;
-                E_ND(i,0)   = r_beads * ND[i + (z - 1) * X - 1]         + (2.00f - 2.00f * r_beads) * ND[i + (z - 1) * X]   + r_beads * ND[i + (z - 1) * X + 1]         + reactionrate1 / P1;
+        for (int z = 1; z < Z; z++) {
+            for (int i = 0; i < X; i++) {
+                reaction_rate_1 = kon1 * dt * (D[i + (z - 1) * X] * NS[i + (z - 1) * X]); // - ND[i + (z - 1) * X] / Keq);
+                reaction_rate_1 = min(reaction_rate_1, NS[i + (z - 1) * X] * P1);
+                reaction_rate_1 = min(D[i + (z - 1) * X], reaction_rate_1);
+                if (i == 0) {
+                    E(i,0)      = (1.00f - r_dye)   * D[i + (z - 1) * X]    + r_dye   * D[i + (z - 1) * X + 1]                                                              - reaction_rate_1;
+                    E_NS(i,0)   = (1.00f - r_beads) * NS[i + (z - 1) * X]   + r_beads * NS[i + (z - 1) * X + 1]                                                             - reaction_rate_1 * P1;
+                    E_ND(i,0)   = (1.00f - r_beads) * ND[i + (z - 1) * X]   + r_beads * ND[i + (z - 1) * X + 1]                                                             + reaction_rate_1;
+                    
+                } else if (i == X - 1) {
+                    E(i,0)      =                                           r_dye   * D[i + (z - 1) * X - 1]                    + (1.00f - r_dye)   * D[i + (z - 1) * X]    - reaction_rate_1;
+                    E_NS(i,0)   =                                           r_beads * NS[i + (z - 1) * X - 1]                   + (1.00f - r_beads) * NS[i + (z - 1) * X]   - reaction_rate_1 * P1;
+                    E_ND(i,0)   =                                           r_beads * ND[i + (z - 1) * X - 1]                   + (1.00f - r_beads) * ND[i + (z - 1) * X]   + reaction_rate_1;
+                } else {
+                    E(i,0)      = r_dye   * D[i + (z - 1) * X - 1]          + (2.00f - 2.00f * r_dye)   * D[i + (z - 1) * X]    + r_dye   * D[i + (z - 1) * X + 1]          - reaction_rate_1;
+                    E_NS(i,0)   = r_beads * NS[i + (z - 1) * X - 1]         + (2.00f - 2.00f * r_beads) * NS[i + (z - 1) * X]   + r_beads * NS[i + (z - 1) * X + 1]         - reaction_rate_1 / P1;
+                    E_ND(i,0)   = r_beads * ND[i + (z - 1) * X - 1]         + (2.00f - 2.00f * r_beads) * ND[i + (z - 1) * X]   + r_beads * ND[i + (z - 1) * X + 1]         + reaction_rate_1;
+                }
             }
-        }
 
-        Solution        = m*E;
-        Solution_DD     = m_beads*E_DD;
-        Solution_NS     = m_beads*E_NS;
-        Solution_ND     = m_beads*E_ND;
-        Solution_NDD    = m_beads*E_NDD;
+            Solution        = m*E;
+            Solution_NS     = m_beads*E_NS;
+            Solution_ND     = m_beads*E_ND;
 
-        for (int i = 0; i < X; i++) {
-            D[i + z * X]    = max(min(Solution(i,0), 1.00f), 0.00f);
-            NS[i + z * X]   = max(min(Solution_NS(i,0), 1.00f), 0.00f);
-            ND[i + z * X]   = max(Solution_ND(i,0), 0.00f);
-            NDD[i + z * X]  = max(Solution_NDD(i,0), 0.00f);
-            DD[i + z * X]   = ND[i + z * X] * P1;
+
+            for (int i = 0; i < X; i++) {
+                D[i + z * X]    = max(min(Solution(i,0), dye_conc), 0.00f);
+                NS[i + z * X]   = max(min(Solution_NS(i,0), bead_conc), 0.00f);
+                ND[i + z * X]   = max(Solution_ND(i,0), 0.00f);
+            }
+            SaveExcelOutput(z-1, D, ND, NS, P1, kon1);
         }
-        
-    }
-    SaveExcelOutput(Z-1, D, DD, ND, NDD, NS);
+    SaveExcelOutput(Z-1, D, ND, NS, P1, kon1);
+    
     std::cout << "Program finished " << endl;
     return 0;
 }
 
 void
-SetInletCondit(float* D, float* DD, float* ND, float* NS, float* NDD, int z)
+SetInletCondit(float* D, float* ND, float* NS, int z)
 {
     int i = 0;
     for (int x = 0; x < X; x++) {
         i = x + X * z;
 
         if (x > (X / 2 - 1)) {
-            D[i] = 1.0f; // mol/m3
+            D[i]  = dye_conc; // molecules / m3
             NS[i] = 0.0f;
         } else {
-            D[i] = 0.0f;
-            NS[i] = 1.0f;
+            D[i]  = 0.0f;
+            NS[i] = bead_conc;
         }
-
-        DD[i] = 0.0f;
         ND[i] = 0.0f;
-        NDD[i] = 0.0f;
     }
 }
 
-void
-SaveViewable(float* V, int X, int Y, int Z, string Filename)
-{
-    ofstream fout;
-    fout.open(Filename);
-    for (int j = 0; j < Y; j++) {
-        for (int i = 0; i < X; i++) {
-            fout << V[i + j * X + Z * X * Y] << "   ";
-        }
-        fout << endl;
-    }
-    fout.close();
-}
 
 void
-SaveExcelOutput(int z, float* D, float* DD, float* ND, float* NDD, float* NS)
+SaveExcelOutput(int z, float* D, float* ND, float* NS, float p, float kon)
 {
     float Sum;
     float* Point;
     ofstream fout;
     fout.open("ExcelProfiles.txt", std::ofstream::out | std::ofstream::app);
-    for (int j = 0; j < 5; j++) {
-        fout << Kon1 << "  " << Kon2 << "   " << Flow << "  " << DyeConc << "   " <<  BeadConc << "  ";
+    for (int j = 0; j < 3; j++) {
+        fout << z * dt << "  " << p << "  " << kon << "  " << dye_conc << "   " <<  bead_conc << "  ";
         if (j == 0) {
             Point = D;
             fout << "D  ";
         }
         if (j == 1) {
-            Point = DD;
-            fout << "DD  ";
+            Point = ND;
+            fout << "ND  ";
         }
         if (j == 2) {
             Point = NS;
             fout << "NS  ";
-        }
-        if (j == 3) {
-            Point = NDD;
-            fout << "NDD  ";
-        }
-        if (j == 4) {
-            Point = ND;
-            fout << "ND ";
         }
         for (int x = 0; x < X; x++) {
             Sum = Point[x + z * X];
